6c050a86f91b92b755d4a0b0bff9208e8a6d8dbe
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-07-14 00:46:47 -0400

    post method

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 const app = express();
 
 const port = 3001;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", async (req, res) => {
   const { data } = await axios.get(
@@ -12,6 +15,13 @@ app.get("/", async (req, res) => {
   res.send(data);
 });
 
+app.post("/", async (req, res) => {
+    console.log(req.body);
+    const { data } = await axios.post(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+    res.send(data);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
