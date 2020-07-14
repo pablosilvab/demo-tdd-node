95df0c695d45ab042c36b952560210b4da1e4346
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-07-14 01:06:34 -0400

    metodos put y delete

--- index.js
+++ index.js
@@ -12,16 +12,36 @@ app.get("/", async (req, res) => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/users"
   );
-  res.send(data);
+  res.status(200).send(data);
 });
 
 app.post("/", async (req, res) => {
-    console.log(req.body);
-    const { data } = await axios.post(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-    res.send(data);
+  console.log(req.body);
+  const { body } = req;
+  const { data } = await axios.post(
+    "https://jsonplaceholder.typicode.com/users",
+    body
+  );
+  res.status(201).send(data);
 });
+
+app.put("/:id", async (req, res) => {
+  const { body } = req;
+  const { id } = req.params;
+  await axios.put(
+    `https://jsonplaceholder.typicode.com/users/${id}`, body 
+  );
+  res.sendStatus(204)
+});
+
+
+app.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    await axios.delete(
+      `https://jsonplaceholder.typicode.com/users/${id}` 
+    );
+    res.sendStatus(204);
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
